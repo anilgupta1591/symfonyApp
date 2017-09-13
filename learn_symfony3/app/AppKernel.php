99941e5cda2019ac6f5b8e9e54b87b9f8c56b1a9
Anil Gupta <anilgupta1591@gmail.com>
2017-09-14 00:24:53 +0530

    installing and configure undles
    fos rest
    fosAuthserver
    serializer bundle

--- learn_symfony3/app/AppKernel.php
+++ learn_symfony3/app/AppKernel.php
@@ -16,6 +16,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new AppBundle\AppBundle(),
         ];
 
---
+++ learn_symfony3/src/AppBundle/Controller/DemoController.php
@@ -0,0 +1,17 @@
+<?php
+
+// src/AppBundle/Controller/DemoController.php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class DemoController extends FOSRestController
+{
+    public function getDemosAction()
+    {
+        $data = array("hello" => "world");
+        $view = $this->view($data);
+        return $this->handleView($view);
+    }
+}
